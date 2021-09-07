fdfc1056ed389accf3c9bd01d05cd2b88be1f1db
Paritosh Kumar <kpari307@gmail.com>
2021-09-07 06:34:13 +0530

    Updates

---
+++ precache-manifest.1cba946ba196289863c5b1285e86c3cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f8f7f31879469113f1857fe959714e",
+    "url": "/viraltrack/index.html"
+  },
+  {
+    "revision": "bb826b6f1f5410251115",
+    "url": "/viraltrack/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "6eb8bc35e2a6cdf241ef",
+    "url": "/viraltrack/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "bb826b6f1f5410251115",
+    "url": "/viraltrack/static/js/2.cc2249d4.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/viraltrack/static/js/2.cc2249d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb8bc35e2a6cdf241ef",
+    "url": "/viraltrack/static/js/main.82113e7f.chunk.js"
+  },
+  {
+    "revision": "beadf329e2f0df6ca009",
+    "url": "/viraltrack/static/js/runtime-main.2e0e7a70.js"
+  }
+]);
